f6a0fe2e0cfcfda530349bb096ba3eafed92cdce
SmolLillia <63683359+PrincessAmi@users.noreply.github.com>
2023-11-24 03:26:04 -0600

    Update setup.py
    Fixed Flask Version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=[
         "dbus-python==1.2.16",
-        "Flask==2.1.3",
+        "Flask==2.0.3",
         "Flask-SocketIO==5.3.4",
         "eventlet==0.33.3",
         "blessed==1.17.10",
